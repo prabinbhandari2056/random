ab7b4e936a4264d59e330c6eac4d8067c01bffeb
Prabin Bhandari <prabinbhandari2056@outlook.com>
2022-12-06 17:42:50 +0545

    First commit

---
+++ customer/pay_offline.php
@@ -0,0 +1,53 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Using Method  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> Esewa , Khalti: </th>
+
+<th> Bank Account Details: </th>
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> Bank Name:NIC Asia Account No:03333333 Branch Code:0003 Branch Name:NIC Asis </td>
+
+<td> NIC#001234567 Mobile No:9874563210, Name: Kamana Machinery </td>
+
+<td> Full Name:Kamana Machinery, Mobile No:9874563210, Name:Avyash Rayamajhi, Country:Nepal, N.I.C No:001234567
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
